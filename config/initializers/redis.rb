aebb619965b0b3a02986b8941184ff5a11a61df0
sathwik-1309 <sathwik1309@gmail.com>
2024-04-02 17:28:50 +0530

    initial changes

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
 require 'redis'
 
-$redis = Redis.new(url: ENV['REDIS_URL'] || 'redis://localhost:6379/2')
+$redis = Redis.new(url: ENV['REDIS_URL_CACHE'] || 'redis://localhost:6379/2')
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
 # config/initializers/sidekiq.rb
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: ENV['REDIS_URL_SIDEKIQ'] || "redis://localhost:6379/0" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: ENV['REDIS_URL_SIDEKIQ']  || "redis://localhost:6379/0" }
 end
 
 # Load custom configuration from sidekiq.yml if present
